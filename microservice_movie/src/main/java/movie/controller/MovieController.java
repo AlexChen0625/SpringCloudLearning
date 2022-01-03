134a384747bf158580220b71ec589ee06da516b8
Alex <alex.chen@mitake.com.tw>
2022-01-03 16:10:55 +0800

    feat: 修改service呼叫方式 使用SpringCloud discoveryClient

--- microservice_movie/src/main/java/movie/controller/MovieController.java
+++ microservice_movie/src/main/java/movie/controller/MovieController.java
@@ -2,16 +2,22 @@ package movie.controller;
 
 import movie.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("movie")
 public class MovieController {
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -22,7 +28,11 @@ public class MovieController {
 	@PostMapping("order")
 	public String order() {
 		String id = "1";
-		User user = restTemplate.getForObject("http://localhost:9001/user/" + id, User.class);
+		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("MICROSERVICE-USER");
+		//目前沒有使用負載均衡 直接使用第一個服務
+		ServiceInstance serviceInstance = serviceInstanceList.get(0);
+
+		User user = restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id, User.class);
 		assert user != null;
 
 		return user.toString();

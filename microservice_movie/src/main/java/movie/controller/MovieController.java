c22469500fcba4183cc49e8cb32d684c5d76f793
Alex <alex.chen@mitake.com.tw>
2022-01-05 14:26:03 +0800

    feat: 調整負載均衡方式

--- microservice_movie/src/main/java/movie/controller/MovieController.java
+++ microservice_movie/src/main/java/movie/controller/MovieController.java
@@ -2,22 +2,23 @@ package movie.controller;
 
 import movie.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("movie")
 public class MovieController {
 
+	//	@Autowired
+	//	private DiscoveryClient discoveryClient;
+
 	@Autowired
-	private DiscoveryClient discoveryClient;
+	private LoadBalancerClient loadBalancerClient;
 	@Autowired
 	private RestTemplate restTemplate;
 
@@ -28,17 +29,22 @@ public class MovieController {
 	@PostMapping("order")
 	public String order() {
 		String id = "1";
-		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("MICROSERVICE-USER");
-		//目前沒有使用負載均衡 直接使用第一個服務
-		ServiceInstance serviceInstance = serviceInstanceList.get(0);
 
-		User user = restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id, User.class);
+		//沒有使用負載均衡 直接使用第一個服務
+		//		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("MICROSERVICE-USER");
+		//		ServiceInstance serviceInstance = serviceInstanceList.get(0);
+
+		//使用Ribbon實現負載均衡
+		//ServiceInstance serviceInstance = loadBalancerClient.choose("MICROSERVICE-USER");
+		User user = restTemplate.getForObject("http://microservice-user/user/" + id, User.class);
+
 		assert user != null;
 
 		return user.toString();
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}

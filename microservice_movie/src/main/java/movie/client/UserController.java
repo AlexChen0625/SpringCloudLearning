aa5d49442453a74f02435ce5e9bd6d47e80ac83d
Alex <alex.chen@mitake.com.tw>
2022-01-09 14:17:22 +0800

    frat: 修改呼叫服務方式為OpenFeign

---
+++ microservice_movie/src/main/java/movie/client/UserController.java
@@ -0,0 +1,19 @@
+package movie.client;
+
+import movie.pojo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 用戶微服務介面
+ * 1. @FeignClient 宣告需要調用的微服務,宣告要呼叫的方法
+ * 2. 檢查Requestmapping路徑是否完整
+ * 3. @Pathvariable需宣告完整參數名稱
+ */
+@FeignClient(value = "microservice-user")
+public interface UserController {
+	@GetMapping(value = "/user/{id}")
+	public User findById(@PathVariable(value = "id") Integer id);
+
+}
--- microservice_movie/src/main/java/movie/controller/MovieController.java
+++ microservice_movie/src/main/java/movie/controller/MovieController.java
@@ -1,51 +1,23 @@
 package movie.controller;
 
+import movie.client.UserController;
 import movie.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("movie")
 public class MovieController {
-
-	//	@Autowired
-	//	private DiscoveryClient discoveryClient;
-
+	
 	@Autowired
-	private LoadBalancerClient loadBalancerClient;
-	@Autowired
-	private RestTemplate restTemplate;
+	private UserController userController;
 
-	/**
-	 * 購票
-	 *
-	 */
-	@PostMapping("order")
-	public String order() {
-		String id = "1";
-
-		//沒有使用負載均衡 直接使用第一個服務
-		//		List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("MICROSERVICE-USER");
-		//		ServiceInstance serviceInstance = serviceInstanceList.get(0);
-
-		//使用Ribbon實現負載均衡
-		//ServiceInstance serviceInstance = loadBalancerClient.choose("MICROSERVICE-USER");
-		User user = restTemplate.getForObject("http://microservice-user/user/" + id, User.class);
-
-		assert user != null;
-
+	@PostMapping("orderByOpenFeign")
+	public String orderByOpenFeign() {
+		Integer id = 1;
+		User user = userController.findById(id);
 		return user.toString();
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }
